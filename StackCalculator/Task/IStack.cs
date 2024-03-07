7cecb6e59c85c478519a01fabfd9f73c780ad149
kamenkremen <ivanz0610@gmail.com>
2024-03-07 18:02:29 +0000

    Add task stack calculator

---
+++ StackCalculator/Task/IStack.cs
@@ -0,0 +1,25 @@
+namespace StackCalculator;
+
+/// <summary>
+/// Represents a first-in-last-out collection of floats.
+/// </summary>
+public interface IStack
+{
+    /// <summary>
+    /// Gets a value indicating whether the stack is empty.
+    /// </summary>
+    int Size { get; }
+
+    /// <summary>
+    /// Returns and remove top element from stack.
+    /// </summary>
+    /// <returns>Last element of the stack and flag, that indicates wheter element is popped or not.</returns>
+    float Pop();
+
+    /// <summary>
+    /// Adds element to the top of the stack.
+    /// </summary>
+    /// <param name="element">Element to add to the stack.</param>
+    void Push(float element);
+
+}
