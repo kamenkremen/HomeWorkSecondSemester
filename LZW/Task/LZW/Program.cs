16e57a7331683c0130fce6853023cd7cd98edeac
kamenkremen <ivanz0610@gmail.com>
2024-04-27 21:16:38 +0000

    Add compare option

--- LZW/Task/LZW/Program.cs
+++ LZW/Task/LZW/Program.cs
@@ -13,9 +13,19 @@ if (path == null)
 
 var command = args[1];
 var withBWT = 0;
-if (args.Length == 3 || args[2] == "-bwt")
+if (args.Length == 3 && args[2] == "-bwt")
 {
     withBWT = 1;
+}
+else if (args.Length == 3 && args[2] == "-compare")
+{
+    withBWT = 2;
+}
+
+if (withBWT == 2 && command == "-u")
+{
+    Console.WriteLine("Can`t compare while encoding, choose with bwt or without it.");
+    return;
 }
 
 switch (command)
@@ -24,11 +34,24 @@ switch (command)
     Console.WriteLine("Encoding...");
     switch (withBWT)
     {
+        case 0:
+        try
+        {
+            double ratio = LZW.Encode(path, false);
+            Console.WriteLine($"Compress ratio without BWT = {ratio}");
+        }
+        catch (ArgumentException exception)
+        {
+            Console.WriteLine($"Error:{exception.Message}");
+            return;
+        }
+
+        break;
         case 1:
         try
         {
-            double ratio = LZW.Encode(path, true);
-            Console.WriteLine($"Compress ratio with BWT = {ratio}");
+            double BWTratio = LZW.Encode(path, true);
+            Console.WriteLine($"Compress ratio with BWT = {BWTratio}");
         }
         catch (ArgumentException exception)
         {
@@ -40,8 +63,9 @@ switch (command)
         case 2:
         try
         {
-            double BWTratio = LZW.Encode(path, true);
-            Console.WriteLine($"Compress ratio with BWT = {BWTratio}");
+            double BWTratioToCompare = LZW.Encode(path, true);
+            double ratioToCompare = LZW.Encode(path, false);
+            Console.WriteLine($"Compress ratio with BWT = {BWTratioToCompare}, ratio without BWT = {ratioToCompare}");
         }
         catch (ArgumentException exception)
         {
@@ -57,10 +81,10 @@ switch (command)
     Console.WriteLine("Decoding...");
     switch (withBWT)
     {
-        case 1:
+        case 0:
         try
         {
-            LZW.Decode(path, true);
+            LZW.Decode(path, false);
             Console.WriteLine("Decoded!");
         }
         catch (ArgumentException exception)
@@ -70,10 +94,10 @@ switch (command)
         }
 
         break;
-        case 2:
+        case 1:
         try
         {
-            LZW.Decode(path, false);
+            LZW.Decode(path, true);
             Console.WriteLine("Decoded!");
         }
         catch (ArgumentException exception)
